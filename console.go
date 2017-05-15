3da8c217892ae1573a9545b6ba36d51979981bce
Justin Cormack <justin.cormack@docker.com>
2017-05-15 08:08:56 +0100

    Use our own simpler recvfd implementation
    This uses cleaner code than the previously imported version, and makes
    this code more portable as the libcontainer/utils library only supports
    Linux due to various assumptions, while this code is portable. Saves an
    import as well.
    
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- console.go
+++ console.go
@@ -10,7 +10,7 @@ import (
 	"path/filepath"
 
 	"github.com/containerd/console"
-	"github.com/opencontainers/runc/libcontainer/utils"
+	"golang.org/x/sys/unix"
 )
 
 // NewConsoleSocket creates a new unix socket at the provided path to accept a
@@ -64,6 +64,50 @@ func (c *Socket) Path() string {
 	return c.path
 }
 
+// recvFd waits for a file descriptor to be sent over the given AF_UNIX
+// socket. The file name of the remote file descriptor will be recreated
+// locally (it is sent as non-auxiliary data in the same payload).
+func recvFd(socket *net.UnixConn) (*os.File, error) {
+	const MaxNameLen = 4096
+	var oobSpace = unix.CmsgSpace(4)
+
+	name := make([]byte, MaxNameLen)
+	oob := make([]byte, oobSpace)
+
+	n, oobn, _, _, err := socket.ReadMsgUnix(name, oob)
+	if err != nil {
+		return nil, err
+	}
+
+	if n >= MaxNameLen || oobn != oobSpace {
+		return nil, fmt.Errorf("recvfd: incorrect number of bytes read (n=%d oobn=%d)", n, oobn)
+	}
+
+	// Truncate.
+	name = name[:n]
+	oob = oob[:oobn]
+
+	scms, err := unix.ParseSocketControlMessage(oob)
+	if err != nil {
+		return nil, err
+	}
+	if len(scms) != 1 {
+		return nil, fmt.Errorf("recvfd: number of SCMs is not 1: %d", len(scms))
+	}
+	scm := scms[0]
+
+	fds, err := unix.ParseUnixRights(&scm)
+	if err != nil {
+		return nil, err
+	}
+	if len(fds) != 1 {
+		return nil, fmt.Errorf("recvfd: number of fds is not 1: %d", len(fds))
+	}
+	fd := uintptr(fds[0])
+
+	return os.NewFile(fd, string(name)), nil
+}
+
 // ReceiveMaster blocks until the socket receives the pty master
 func (c *Socket) ReceiveMaster() (console.Console, error) {
 	conn, err := c.l.Accept()
@@ -71,15 +115,11 @@ func (c *Socket) ReceiveMaster() (console.Console, error) {
 		return nil, err
 	}
 	defer conn.Close()
-	unix, ok := conn.(*net.UnixConn)
+	uc, ok := conn.(*net.UnixConn)
 	if !ok {
 		return nil, fmt.Errorf("received connection which was not a unix socket")
 	}
-	sock, err := unix.File()
-	if err != nil {
-		return nil, err
-	}
-	f, err := utils.RecvFd(sock)
+	f, err := recvFd(uc)
 	if err != nil {
 		return nil, err
 	}
